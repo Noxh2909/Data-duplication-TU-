48cf4017b93238c40d340cf444805cd71b85541b
Noxh2909 <noah.kogge@gmail.com>
2024-05-22 00:09:12 +0200

    29,5% f1 score, similarity treshold increased to 0.7 for better accuarcy

--- dedup_z2.py
+++ dedup_z2.py
@@ -1,9 +1,3 @@
-"""
-You should implement the functions in this file.
-The goal is to provide you with the basic structure.
-Feel free to create additional functions.
-You are not limited to a single blocking or matching function.
-"""
 import time
 from collections import defaultdict
 from tqdm import tqdm
@@ -11,67 +5,66 @@ import pandas as pd
 import numpy as np
 import re
 
-name_column_name = 1 
+patterns = {
+    'name': [
+        r'&(nbsp|amp|reg|[a-z]?acute|quot|trade);?|[|;:/,‰+©\(\)\\][psn]*|(?<=usb)[\s][m]*(?=[23][\.\s])|(?<=usb)-[\w]+\s(?=[23][\.\s])|(?<=[a-z])[\s]+gb|(?<=data|jump)[t\s](?=trave|drive)|(?<=extreme|exceria)[\s](?=pro[\s]|plus)|(?<=class)[\s_](?=10|[234]\b)|(?<=gen)[\s_](?=[134\s][0]*)',
+        r'(10 class|class 10|class(?=[\w]+10\b)|cl\s10)',
+        r'\b(msd|microvault|sd-karte|speicherkarte|minneskort|memóriakártya|flashgeheugenkaart|geheugenkaart|speicherkarten|memoriakartya|[-\s]+kaart|memory|memoria|memoire|mémoire|mamoria|tarjeta|carte|karta)',
+        r'\b(flash[\s-]*drive|flash[\s-]*disk|pen[\s]*drive|micro-usb|usb-flashstation|usb-flash|usb-minne|usb-stick|speicherstick|flashgeheugen|flash|vault)',
+        r'\b(adapter|adaptateur|adaptador|adattatore)',
+        r'silver|white|black|blue|purple|burgundy|red|green',
+        r'\b[0-9]{2,3}r[0-9]{2,3}w',
+        r'\b([\(]*[\w]+[-]*[\d]+[-]*[\w]+[-]*[\d+]*|[\d]+[\w]|[\w][\d]+)',
+        r'\b(intenso|lexar|logilink|pny|samsung|sandisk|kingston|sony|toshiba|transcend)\b',
+        r'\b(datatraveler|extreme[p]?|exceria[p]?|dual[\s]*(?!=sim)|evo|xqd|ssd|cruzer[\w+]*|glide|blade|basic|fit|force|basic line|jump\s?drive|hxs|rainbow|speed line|premium line|att4|attach|serie u|r-serie|beast|fury|impact|a400|sd[hx]c|uhs[i12][i1]*|note\s?9|ultra)',
+        r'\b(tv|(?<=dual[\s-])*sim|lte|[45]g\b|[oq]*led_[u]*hd|led|galaxy|iphone|oneplus|[0-9]{1,2}[.]*[0-9]*(?=[-\s]*["inch]+))',
+        r'([1-9]{1,3})[-\s]*[g][bo]?',
+        r'(thn-[a-z][\w]+|ljd[\w+][-][\w]+|ljd[sc][\w]+[-][\w]+|lsdmi[\d]+[\w]+|lsd[0-9]{1,3}[gb]+[\w]+|ljds[0-9]{2}[-][\w]+|usm[0-9]{1,3}[\w]+|sdsq[a-z]+[-][0-9]+[a-z]+[-][\w]+|sdsd[a-z]+[-][0-9]+[\w]+[-]*[\w]*|sdcz[\w]+|mk[\d]+|sr-g1[\w]+)',
+        r'\b(c20[mc]|sda[0-9]{1,2}|g1ux|s[72][05]|[unm][23]02|p20|g4|dt101|se9|[asm][0-9]{2})',
+        r'\b(usb[23]|type-c|uhs[i]{1,2}|class[0134]{1,2}|gen[1-9]{1,2}|u[23](?=[\s\.])|sd[hx]c|otg|lte|[45]g[-\s]lte|[0-9]+(?=-inch)|[0-9]{2,3}r[0-9]{2,3}w|[0-9]{2,3}(?=[\smbo/p]{3}))'
+    ],
+    'brand': [
+        r'\b(intenso|lexar|logilink|pny|samsung|sandisk|kingston|sony|toshiba|transcend)\b'
+    ]
+}
 
-def generate_blocking_key(row: pd.Series):
-    """Generate a blocking key based on the 'name', 'brand'"""
-    patterns = {
-        'name': [
-            r"\b[A-Z0-9]{2,10}-[A-Z0-9]{2,10}\b",                   # Specific product codes
-            r"\b\w+\b",                                             # Any word
-            r"[A-Za-z]+\d+",                                        # Alphanumeric sequences
-            r"\d+[A-Za-z]+",                                        # Numeric followed by alphabets
-            r"[A-Za-z]+[^\w\s]+",                                   # Words with special characters
-            r"[^\w\s]+[A-Za-z]+",                                   # Special characters followed by words
-            r"\d+",                                                 # Numeric sequences
-            r'silver|white|black|blue|purple|burgundy|red|green'
-        ],
-        'brand': [
-            r"\b\w+\b",                                             # Any word
-            r"\b[A-Za-z]{2,}\b",                                    # Brand name with at least two characters
-            r"[A-Za-z]+\d+",                                        # Alphanumeric sequences in brand names
-            r"\d+[A-Za-z]+",                                        # Numeric followed by alphabets in brand names
-        ]
-    }
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^\w\s]', '', text)
+    return text
+
+def generate_blocking_key_name(row: pd.Series):
     keys = []
     
+    # Process name field
     if pd.notna(row['name']):
+        name_cleaned = clean_text(str(row['name']))
         for pattern in patterns['name']:
-            matches = re.findall(pattern, str(row['name']), re.IGNORECASE)
+            matches = re.findall(pattern, name_cleaned)
             keys.extend([match.lower() for match in matches])
     
+    # Process brand field
     if pd.notna(row['brand']):
+        brand_cleaned = clean_text(str(row['brand']))
         for pattern in patterns['brand']:
-            matches = re.findall(pattern, str(row['brand']), re.IGNORECASE)
+            matches = re.findall(pattern, brand_cleaned)
             keys.extend([match.lower() for match in matches])
-    
+
     if not keys:
         return ''
-    return ' '.join(sorted(set(keys))) 
+    
+    return ' '.join(sorted(set(keys)))
 
 def create_blocks(df: pd.DataFrame):
-    '''
-    This function creates maps prodicts into blocks based on the blocking key
-    :param df: the input data containing the information about the products
-    :return: a dictionary that the key is the generated blocking key and the value is the product id
-    '''
-
     blocks = defaultdict(list)
     for rowid in tqdm(range(df.shape[0])):
-        blocking_key = generate_blocking_key(df.iloc[rowid,:])
+        blocking_key = generate_blocking_key_name(df.iloc[rowid, :])
         if blocking_key != '':
             blocks[blocking_key].append(rowid)
-
+        
     return blocks
 
 def generate_matches(blocks: defaultdict, df: pd.DataFrame):
-    '''
-    based on the created blocks, this function generates candidate matches
-    :param blocks: the blocks where the key represents the blocking key and the value is the product ids
-    :param df: the input data containing the information about the products
-    :return: a list of tuples, where each tuple is a pair of product id, that are returned as candidate matches
-    '''
-
     candidate_pairs = []
     for key in tqdm(blocks):
         row_ids = list(sorted(blocks[key]))
@@ -80,7 +73,7 @@ def generate_matches(blocks: defaultdict, df: pd.DataFrame):
                 for j in range(i + 1, len(row_ids)):
                     candidate_pairs.append((row_ids[i], row_ids[j]))
 
-    similarity_threshold = .5
+    similarity_threshold = 0.7
     jaccard_similarities = []
     candidate_pairs_product_ids = []
     for it in tqdm(candidate_pairs):
@@ -89,28 +82,24 @@ def generate_matches(blocks: defaultdict, df: pd.DataFrame):
         # get product ids
         product_id1 = df['id'][id1]
         product_id2 = df['id'][id2]
-        if product_id1 < product_id2:  # NOTE: This is to make sure in the final candidates, for a pair id1 and id2 (assume id1<id2), we only include (id1,id2) but not (id2, id1)
+        if product_id1 < product_id2:  # Ensure order
             candidate_pairs_product_ids.append((product_id1, product_id2))
         else:
             candidate_pairs_product_ids.append((product_id2, product_id1))
 
         # compute jaccard similarity
-        name1 = str(df[df.columns.values[1]][id1])
-        name2 = str(df[df.columns.values[1]][id2])
+        name1 = str(df['name'][id1])
+        name2 = str(df['name'][id2])
         s1 = set(name1.lower().split())
         s2 = set(name2.lower().split())
         jaccard_similarities.append(len(s1.intersection(s2)) / max(len(s1), len(s2)))
-    candidate_pairs_product_ids = [x for s, x in sorted(zip(jaccard_similarities, candidate_pairs_product_ids), reverse=True) if s >= similarity_threshold]
+
+    candidate_pairs_product_ids = [
+        x for s, x in sorted(zip(jaccard_similarities, candidate_pairs_product_ids), reverse=True) if s >= similarity_threshold
+    ]
     return candidate_pairs_product_ids
 
-
 def evaluate(match_pairs: list, ground_truth: pd.DataFrame):
-    '''
-    Calculates the precision, recall, and F1 score
-    :param candidate_pairs: list of candidate pairs
-    :param ground_truth: the dataframe containing the actual matches
-    :return: Does not return anything
-    '''
     gt = list(zip(ground_truth['lid'], ground_truth['rid']))
     tp = len(set(match_pairs).intersection(set(gt)))
     fp = len(set(match_pairs).difference(set(gt)))
@@ -119,32 +108,28 @@ def evaluate(match_pairs: list, ground_truth: pd.DataFrame):
     precision = tp / (tp + fp)
 
     print(f'Reported # of Pairs: {len(match_pairs)}')
-    print('TP: {}'.format(tp))
-    print('FP: {}'.format(fp))
-    print('FN: {}'.format(fn))
-    print('Recal: {}'.format(recall))
-    print('Precision: {}'.format(precision))
-    print('F1: {}'.format((2 * precision * recall) / (precision + recall)))
+    print(f'TP: {tp}')
+    print(f'FP: {fp}')
+    print(f'FN: {fn}')
+    print(f'Recall: {recall}')
+    print(f'Precision: {precision}')
+    print(f'F1: {(2 * precision * recall) / (precision + recall)}')
 
-
-# read the datasets
-# Z1 = pd.read_csv("Data/Z1.csv")
+# read the dataset
 Z2 = pd.read_csv("Data/Z2.csv")
 
 starting_time = time.time()
-# perform blocking
-# blocks_Z1 = Z1_candidate_pairs = create_blocks(Z1)
-blocks_Z2 = Z2_candidate_pairs = create_blocks(Z2)
+
+# Perform blocking
+blocks_Z2 = create_blocks(Z2)
 
 # Generate candidates
-# candidates_Z1 = generate_matches(blocks_Z1, Z1)
 candidates_Z2 = generate_matches(blocks_Z2, Z2)
+
 ending_time = time.time()
 
 # Evaluation
 print('------------- Evaluation Results --------------')
 print(f'Runtime: {ending_time - starting_time} Seconds')
-# print('------------- First Dataset --------------')
-# evaluate(candidates_Z1, pd.read_csv('Data/ZY1.csv'))
 print('------------- Second Dataset --------------')
 evaluate(candidates_Z2, pd.read_csv('Data/ZY2.csv'))
